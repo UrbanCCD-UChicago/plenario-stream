53cf1630850cd011faba3d4381863044ed1baf3d
Will Engler <Engler.Will@gmail.com>
2016-06-09 12:42:43 -0500

    Run socket.io sample app

---
+++ ProcessKinesisRecords.js
@@ -0,0 +1,11 @@
+console.log('Loading function');
+
+exports.handler = function(event, context, callback) {
+    //console.log(JSON.stringify(event, null, 2));
+    event.Records.forEach(function(record) {
+        // Kinesis data is base64 encoded so decode here
+        var payload = new Buffer(record.kinesis.data, 'base64').toString('ascii');
+        console.log('Decoded payload:', payload);
+    });
+    callback(null, "message");
+};
