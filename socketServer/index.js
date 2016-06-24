d97e7bc2a80e1d0f2c97f0203bf0e19503775a69
Ubuntu <ubuntu@ip-10-0-0-185.ec2.internal>
2016-06-24 18:33:23 +0000

    Prep for KCL integration

--- socketServer/index.js
+++ socketServer/index.js
@@ -1,19 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var redis = require('socket.io-redis');
-io.adapter(redis({ host: 'localhost', port: 6379 }));
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+    socket.on('message', function(msg){
+        io.emit('message', msg);
+    });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(80, function(){
+    console.log('listening on http://54.152.59.142/');
 });
