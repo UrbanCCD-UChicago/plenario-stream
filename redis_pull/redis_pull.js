d9cf7a29d993716aad85ccb9f5a87b35e6e9d11f
pbeckman <pbeckman@uchicago.edu>
2016-07-05 19:20:04 +0000

    final past-hour-dump protoype

--- redis_pull/redis_pull.js
+++ redis_pull/redis_pull.js
@@ -1,72 +1,53 @@
+
 // Uses 'id' from observations not metadata
 // Assumes observation lists contain data from only one node ID
 
 var endpoint = 'plenario-cache-001.eucixb.0001.use1.cache.amazonaws.com',
     redis = require('redis'),
-    materialize = require('../summarizer/materialize');
     promise = require('promise');
 
 var pull_node = function(id) {
-        var client = redis.createClient(6379, endpoint);
-        var prom = new promise(function(resolve, reject) {
-                client.get(id, function(err, value) {
-                        client.quit();
-                        if(value != null){
-                                resolve(value);
+    var client = redis.createClient(6379, endpoint);
+    var prom = new promise(function(fulfill, reject) {
+            client.get(id, function(err, value) {
+                    client.quit();
+                    if(value != null){
+                            fulfill(value);
                         }
-                        else {
-                                reject('No data found for that node ID');
+                    else {
+                            reject('No data found for that node ID');
                         }
                 });
         });
-        return prom
+    return prom
 };
 
 var update_node = function(observation) {
-	var client = redis.createClient(6379, endpoint);
-	var prom = new promise(function(resolve, reject) {
+    var client = redis.createClient(6379, endpoint);
+    var prom = new promise(function(fulfill, reject) {
 	client.get(observation['id'], function(err, value) {
-		if(value != null){
-			var value_JSON = JSON.parse(value);
-			value_JSON['Last_hour'].push(observation);
-			client.set(observation['id'],JSON.stringify(value_JSON),function(err, reply){
-				client.quit();
-				resolve();
-			});
+	    if(value != null){
+		var value_JSON = JSON.parse(value);
+		while(value_JSON['Last_hour'][0]['time'] < (Date.now()/1000 - 3600)) {
+		    value_JSON['Last_hour'].splice(0,1);
 		}
-		else{
-			client.set(observation['id'],JSON.stringify({'Last_hour': [observation]}),function(err, reply){
-				client.quit();
-				resolve();
-                        });
-		}
+		value_JSON['Last_hour'].push(observation);
+		client.set(observation['id'],JSON.stringify(value_JSON),function(err, reply){
+			client.quit();
+			fulfill();
+		    });
+	    }
+	    else {
+		client.set(observation['id'],JSON.stringify({'Last_hour': [observation]}),function(err, reply){
+		    client.quit();
+		    fulfill();
+                });
+	    }
 	});
     });
-	return prom;
+    return prom;
 };
 
-// updates aggregate from list of observations
-/*
-var update_node = function(observations,metadata) {
-        var client = redis.createClient(6379, endpoint);
-        var prom = new promise(function(resolve, reject) {
-        client.get(observations[0]['id'], function(err, value) {
-                if(value != null){
-                        client.set(observations[0]['id'],JSON.stringify(materialize.update_node_summary(JSON.parse(value),observations,metadata)),function(err, reply){
-                                client.quit();
-                                resolve();
-                        });
-                }
-                else{
-                        client.set(observations[0]['id'],JSON.stringify(materialize.make_node_summary(observations,metadata)),function(err, reply){
-                                client.quit();
-                                resolve();
-                        });
-                }
-        });
-    });
-        return prom;
-};
-*/
 exports.update_node = update_node;
 exports.pull_node = pull_node;
+
